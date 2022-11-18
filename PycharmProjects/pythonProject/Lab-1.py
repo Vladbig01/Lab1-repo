cad8fedb5271975130c24ff430c31cb064fdbfe1
Vladbig01 <118096431+Vladbig01@users.noreply.github.com>
2022-11-18 18:46:46 +0600

    Update Lab-1.py

--- PycharmProjects/pythonProject/Lab-1.py
+++ PycharmProjects/pythonProject/Lab-1.py
@@ -1,21 +1,22 @@
+# 1 Гипотенуза
 a = int(input())
 b = int(input())
 c = (a * a) + (b * b)
 print(c ** 0.5)
 
 
-
+# 2 Число десятков
 a = int(input())
 b = a // 10 % 10
 print(b)
 
 
-
+# 3 Следующее чётное
 a = int(input())
 print(a + 2 - (a % 2))
 
 
-
+# 5 Какое из чисел больше
 a = int(input())
 b = int(input())
 if a > b:
@@ -26,23 +27,14 @@ else:
     print(0)
 
 
-
+# 6 Максимум из трёх
 a = int(input())
 b = int(input())
 c = int(input())
 print(max(a, b, c))
 
 
-
-a = int(input())
-b = int(input())
-c = int(input())
-if a > b:
-    print(a) if a > c else print(c)
-    print(b) if b > c else print(c)
-
-
-
+# 8 Существует ли треугольник
 a = int(input())
 b = int(input())
 c = int(input())
@@ -52,7 +44,19 @@ else:
     print('NO')
 
 
+# 9 Количество равных из трёх
+a = int(input())
+b = int(input())
+c = int(input())
+if a == b == c:
+   print(3)
+elif a == b or b == c or a == c:
+   print(2)
+else:
+   print(0)    
 
+
+# 10 Упорядочить три числа
 a = int(input())
 b = int(input())
 c = int(input())
